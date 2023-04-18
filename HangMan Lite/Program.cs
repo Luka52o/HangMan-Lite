0c83fa9e343f8acc40d0aa64711f17bca68ca598
Luka Silic <silicluka520@gmail.com>
2023-04-18 11:05:12 -0400

    Finding the best way to loop through the word to replace _s with chars in the correct position for words that have letters that appear more than once.

--- HangMan Lite/Program.cs
+++ HangMan Lite/Program.cs
@@ -4,32 +4,80 @@
     {
         static void Main(string[] args)
         {             
-            string word = "COMPUTER", displayWord = "________", guess;
-            int incorrectGuesses;
+            string word, displayWord = "", guess;
+            int incorrectGuesses = 0, correctGuesses = 0, guessIndex = 0;
             bool done = false;
 
             Console.WriteLine("Welcome to Hangman!");
+            Console.WriteLine("Please have a second player enter the secret word, or enter '0' to use a random word");
+            word = Console.ReadLine();
+            if (word == "0")
+            {
+                // randomly generate word
+            }
+            
             DrawStageOne();
 
             Console.WriteLine();
+            for (int i = 0; i < word.Length; i++)
+            {
+                displayWord = displayWord + "_";
+            }
             Console.WriteLine(displayWord);
             while (!done)
             {
                 Console.WriteLine("Please enter a letter to guess:");
                 guess = Console.ReadLine();
-                if (word.Contains(guess.ToUpper()))
+                if (word.Contains(guess.ToUpper()) && guess.ToUpper() != "")
                 {
                     Console.WriteLine("CORRECT! :)");
+                    correctGuesses++;
                     Console.WriteLine();
-                    Console.WriteLine(displayWord.Replace(word.IndexOf(guess.ToUpper()), guess.ToUpper())); // this is not how .Replace() works, it works by (char in string, new char) not by index :(
+                    for (int i = 0; i < word.Length; i++)
+                    {
+                        guessIndex = word.IndexOf(guess.ToUpper(), guessIndex + 1);
+                        displayWord = displayWord.Remove(guessIndex, 1);
+                        displayWord = displayWord.Insert(guessIndex, guess.ToUpper());
+                    }
+                    
+                    Console.WriteLine(displayWord);
                 }
                 else
                 {
                     Console.WriteLine("INCORRECT! :(");
+                    incorrectGuesses++;
+                    if (incorrectGuesses == 1)
+                    {
+                        DrawStageTwo();
+                    }
+                    else if (incorrectGuesses == 2)
+                    {
+                        DrawStageThree();
+                    }
+                    else if (incorrectGuesses == 3)
+                    {
+                        DrawStageFour();
+                    }
+                    else if (incorrectGuesses == 4)
+                    {
+                        DrawStageFive();
+                    }
+                    else if (incorrectGuesses == 5)
+                    {
+                        DrawStageSix();
+                    }
+                    else if (incorrectGuesses == 6)
+                    {
+                        DrawStageSeven();
+                    }
+                    else if (incorrectGuesses == 7)
+                    {
+                        Console.WriteLine("YOU DIED");
+                        done = true;
+                    }
+                   
                 }
             }
-
-
         }
 
 
@@ -58,7 +106,7 @@
             Console.WriteLine(@" +---+");
             Console.WriteLine(@" |   |");
             Console.WriteLine(@" O   |");
-            Console.WriteLine(@" |  |");
+            Console.WriteLine(@" |   |");
             Console.WriteLine(@"     |");
             Console.WriteLine(@"     |");
             Console.WriteLine(@"======");
@@ -68,8 +116,8 @@
             Console.WriteLine(@" +---+");
             Console.WriteLine(@" |   |");
             Console.WriteLine(@" O   |");
-            Console.WriteLine(@"/|  |");
-            Console.WriteLine(@"    |");
+            Console.WriteLine(@"/|   |");
+            Console.WriteLine(@"     |");
             Console.WriteLine(@"     |");
             Console.WriteLine(@"======");
         }
