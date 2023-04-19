4f20d197bc44dd8e95c12c7ed319d37a6ca4cba5
Luka Silic <silicluka520@gmail.com>
2023-04-19 11:09:15 -0400

    finished the program

--- HangMan Lite/Program.cs
+++ HangMan Lite/Program.cs
@@ -1,16 +1,20 @@
-﻿namespace HangMan_Lite
+﻿using System.Globalization;
+
+namespace HangMan_Lite
 {
     internal class Program
     {
         static void Main(string[] args)
         {             
             string word, displayWord = "", guess;
-            int incorrectGuesses = 0, correctGuesses = 0, guessIndex = 0;
+            int incorrectGuesses = 0, correctGuesses = 0, guessIndex = 0, guessIndexInitial;
             bool done = false;
+            List<string> guesses = new List<string>();
 
             Console.WriteLine("Welcome to Hangman!");
             Console.WriteLine("Please have a second player enter the secret word, or enter '0' to use a random word");
             word = Console.ReadLine();
+            word = word.ToUpper();
             if (word == "0")
             {
                 // randomly generate word
@@ -19,6 +23,7 @@
             DrawStageOne();
 
             Console.WriteLine();
+            Console.WriteLine("WORD:");
             for (int i = 0; i < word.Length; i++)
             {
                 displayWord = displayWord + "_";
@@ -28,24 +33,57 @@
             {
                 Console.WriteLine("Please enter a letter to guess:");
                 guess = Console.ReadLine();
-                if (word.Contains(guess.ToUpper()) && guess.ToUpper() != "")
+                guess = guess.ToUpper();
+                if (word.Contains(guess) && !guesses.Contains(guess) && guess != "") 
                 {
                     Console.WriteLine("CORRECT! :)");
-                    correctGuesses++;
+                    guessIndex = word.IndexOf(guess);
+                    guessIndexInitial = guessIndex;
                     Console.WriteLine();
-                    for (int i = 0; i < word.Length; i++)
+                    for (int i = 0; i < word.Length - (guessIndexInitial - 1); i++)
                     {
-                        guessIndex = word.IndexOf(guess.ToUpper(), guessIndex + 1);
-                        displayWord = displayWord.Remove(guessIndex, 1);
-                        displayWord = displayWord.Insert(guessIndex, guess.ToUpper());
+                        // for loop goes through each character in the string, and if the character is there, the guessIndex is used to remove the char slot and replace it with the guessed character
+                        if (guessIndex != -1 && guess != "")
+                        {
+
+                            displayWord = displayWord.Remove(guessIndex, 1);
+                            displayWord = displayWord.Insert(guessIndex, guess);
+                            guessIndex = word.IndexOf(guess, guessIndex + 1);
+                        }
                     }
-                    
                     Console.WriteLine(displayWord);
+                    if (guess != "")
+                    {
+                        guesses.Add(guess);
+                    }
+                    Console.WriteLine();
+                    Console.WriteLine("Guesses: ");
+                    for (int i = 0; i < guesses.Count; i++)
+                    {
+                        Console.Write($"{guesses[i]}, ");
+                    }
+                    Console.WriteLine();
+                    if (!displayWord.Contains("_"))
+                    {
+                        done = true;
+                    }
+                }
+                else if (guesses.Contains(guess))
+                {
+                    Console.WriteLine("You've already guessed that letter");
                 }
                 else
                 {
                     Console.WriteLine("INCORRECT! :(");
                     incorrectGuesses++;
+                    guesses.Add(guess);
+                    Console.WriteLine("Guesses: ");
+                    for (int i = 0; i < guesses.Count; i++)
+                    {
+                        Console.Write($"{guesses[i]}, ");
+                    }
+                    Console.WriteLine();
+                    Console.WriteLine($"{7 - incorrectGuesses} lives left!");
                     if (incorrectGuesses == 1)
                     {
                         DrawStageTwo();
@@ -73,11 +111,17 @@
                     else if (incorrectGuesses == 7)
                     {
                         Console.WriteLine("YOU DIED");
-                        done = true;
+                        Console.WriteLine("PRESS ENTER TO CLOSE THE APPLICATION");
+                        Console.ReadLine();
+                        System.Environment.Exit(0);
                     }
                    
                 }
             }
+            Console.WriteLine("YOU WIN!");
+            Console.WriteLine("PRESS ENTER TO CLOSE THE APPLICATION");
+            Console.ReadLine();
+            System.Environment.Exit(0);
         }
 
 
